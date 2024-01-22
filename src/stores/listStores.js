b4c343b1a0824fe0f96dfe319a7bf914f0185c90
MorganGraves <gravesm@hawkeye.fastmail.net>
2024-01-22 15:46:02 -0600

    Working on listStores, using dicts and not arrays

--- src/stores/listStores.js
+++ src/stores/listStores.js
@@ -2,12 +2,18 @@ import ShortUniqueId from 'short-unique-id';
 import { reactive } from 'vue';
 
 export const listsData = reactive({
-  tabs: [], // This will be a 2d array
-  addTab: () => {
+  // tabs: [], // This will be a 2d array
+  //I think this will work better using a dictionary so that the tab labels can act as the keys
+  tabDict: {
+    'Tasks': ['Your first task', 'your second task']
+  },
+  addTab: (tabLabel, item) => {
     // add a new tab
+    tabDict[tabLabel] = [item]
   },
   addItem: (tab) => {
     // add an item to the specified tab
+    
   },
   deleteTab: (tab) => {
     // delete the specified tab
