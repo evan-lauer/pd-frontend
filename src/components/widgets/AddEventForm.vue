03a318ad2d434ab4b58ef5bdf5c0bddf923ebc6d
Evan Lauer <lauerevan@gmail.com>
2024-01-24 17:35:31 -0600

    Working on time pickers

--- src/components/widgets/AddEventForm.vue
+++ src/components/widgets/AddEventForm.vue
@@ -1,9 +1,56 @@
-<script setup></script>
+<script setup>
+import TimePicker from 'src/components/widgets/TimePicker.vue';
+import { selectedDate } from 'src/stores/calendarStores';
+import { ref } from 'vue';
+
+// Fill this object with the original timestamp of the current selected time (truncated to nearest hour)
+const startTime = ref({
+  hours:
+    selectedDate.dateTime.getHours() > 12
+      ? selectedDate.dateTime.getHours() - 12
+      : selectedDate.dateTime.getHours(),
+  minutes: 0,
+  amOrPm: selectedDate.dateTime.getHours() >= 12 ? 'pm' : 'am'
+});
+const oneHourAhead = new Date(
+  new Date(selectedDate.dateTime).setTime(selectedDate.dateTime.getTime() + 60 * 60 * 1000)
+);
+// Fill this object with a timestamp one hour ahead of selected time (truncated to nearest hour)
+const endTime = ref({
+  hours: oneHourAhead.getHours() + 1 > 12 ? oneHourAhead.getHours() - 12 : oneHourAhead.getHours(),
+  minutes: 0,
+  amOrPm: oneHourAhead.getHours() >= 12 ? 'pm' : 'am'
+});
+</script>
 <template>
   <div class="formContainer">
     <h1 class="formTitleHeader">Create Event</h1>
     <input class="titleInput" />
     <input class="descriptionInput" />
+    <div class="datePickerPanel">
+      <div class="inputRow date">
+        Start Date
+        <input
+          class="datePicker start"
+          type="date"
+        />
+      </div>
+      <div class="inputRow time">
+        Start Time
+        <TimePicker :timestamp="startTime" />
+      </div>
+      <div class="inputRow date">
+        End Date
+        <input
+          class="datePicker end"
+          type="date"
+        />
+      </div>
+      <div class="inputRow time">
+        End Time
+        <TimePicker :timestamp="endTime" />
+      </div>
+    </div>
   </div>
 </template>
 <style scoped>
@@ -17,5 +64,16 @@
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  gap: 5px;
+}
+
+.datePickerPanel {
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+}
+.inputRow {
+  display: flex;
+  gap: 8px;
 }
 </style>
---
+++ src/components/widgets/TimePicker.vue
@@ -0,0 +1,67 @@
+<script setup>
+import { ref } from 'vue';
+defineProps({
+  timestamp: {
+    hour: Number,
+    minute: Number,
+    amOrPm: String
+  }
+});
+
+const selectedField = ref('none');
+// When the input changes we will emit a "time-change" event with arguments hours, minutes, amOrPm
+</script>
+<template>
+  <div class="timePickerContainer">
+    <div
+      :class="selectedField === 'hours' ? `selected hours field` : `hours field`"
+      @click="selectedField = 'hours'"
+    >
+      <p class="typography">
+        {{ timestamp.hours < 10 ? `0` + timestamp.hours : timestamp.hours }}
+      </p>
+    </div>
+    <div
+      :class="selectedField === 'minutes' ? `selected minutes field` : `minutes field`"
+      @click="selectedField = 'minutes'"
+    >
+      <p class="typography">
+        {{ timestamp.minutes < 10 ? `0` + timestamp.minutes : timestamp.minutes }}
+      </p>
+    </div>
+    <div
+      :class="selectedField === 'amOrPm' ? `selected amOrPm field` : `amOrPm field`"
+      @click="selectedField = 'amOrPm'"
+    >
+      <p class="typography">
+        {{ timestamp.amOrPm }}
+      </p>
+    </div>
+  </div>
+</template>
+<style scoped>
+.timePickerContainer {
+  height: 20px;
+  width: 80px;
+  display: flex;
+  gap: 5px;
+}
+
+.field {
+  height: 100%;
+  width: 100%;
+  border: 1px solid black;
+  border-radius: 4px;
+}
+
+.field:hover {
+  cursor: default;
+}
+
+.typography {
+  margin: 0;
+}
+
+.selected .typography {
+}
+</style>
