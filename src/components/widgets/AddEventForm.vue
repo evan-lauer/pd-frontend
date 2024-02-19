1135fc51074c236bb6935dd5d9e9225c8f2d83ac
Shoko Ishikawa <shokoishikawa@Shokos-MacBook.local>
2024-02-18 19:22:54 -0600

    added icons and modified how TimePicker is called

--- src/components/widgets/AddEventForm.vue
+++ src/components/widgets/AddEventForm.vue
@@ -13,82 +13,63 @@ function submissionHandler() {
 
 <template>
   <div class="formContainer">
-    <h1 class="formTitleHeader">Create Event</h1>
-    <input class="titleInput" />
-    <input class="descriptionInput" />
-    <div class="datePickerPanel">
-      <div class="inputRow date">
-        Start Date
+      <div class="inputRow">
+        <input class="eventNameInput" placeholder="Add Event Title"/>
+      </div>
+      <div class="inputRow">
+        <img src="src/components/icons/clock9.svg" class="clockIcon" alt="Time Icon">
+        <div class="dateLabel">Start Date</div>
         <input
-          class="datePicker start"
+          class="datePicker"
           type="date"
           :value="addEventForm.startDateTime"
         />
-      </div>
-      <div class="inputRow time">
-        Start Time
         <TimePicker :timestamp="addEventForm.startDateTime" />
       </div>
-      <div class="inputRow date">
-        End Date
+      <div class="inputRow">
+        <div class="dateLabel">End Date</div>
         <input
-          class="datePicker end"
+          class="datePicker"
           type="date"
           :value="addEventForm.endDateTime"
         />
-      </div>
-      <div class="inputRow time">
-        End Time
         <TimePicker :timestamp="addEventForm.endDateTime" />
       </div>
+      <div class="inputRow">
+        <img src="src/components/icons/description.svg" class="descriptionIcon" alt="Description Icon">
+        <input class="descriptionInput" placeholder="Add Event Description" />
+      </div>
+      <div class="saveButtonRow">
+        <SimpleButton
+          inner-text="Save"
+          @click="submissionHandler()"
+        />
+      </div>
     </div>
-    <div class="inputRow">
-      <p class="dateLabel">End Date</p>
-      <input
-        id="endDate"
-        class="datePicker"
-        type="date"
-        v-bind:value="addEventForm.endDateTime"
-      />
-      <TimePicker
-        v-if="addEventForm.isPrefilled"
-        :timestamp="prefilledEndTime"
-      />
-      <TimePicker
-        v-else
-        :timestamp="addEventForm.endDateTime"
-      />
-    </div>
-    <div class="inputRow">
-      <input
-        class="descriptionInput"
-        placeholder="Add Event Description"
-      />
-    </div>
-    <div class="saveButtonRow">
-      <SimpleButton
-        inner-text="Submit"
-        @click="submissionHandler()"
-      />
-    </div>
-  </div>
 </template>
 
 <style scoped>
-.formTitleHeader {
-  font-size: large;
-  display: flex;
-  justify-content: center;
-}
 .formContainer {
   height: 100%;
-  width: 100%;
+  width: 95%;
   display: flex;
-  flex-direction: column;
-  align-items: stretch;
-  justify-content: space-evenly;
   gap: 5px;
-  padding: 2px;
+  margin: 0 0 0 20px;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: stretch;
+}
+.clockIcon {
+  height: 15px;
+  width: 15px;
+  position: absolute;
+  left: 25px;  
+}
+.descriptionIcon {
+  height: 15px;
+  width: 15px;
+  position: absolute;
+  left: 25px;  
 }
 .eventNameInput {
   height: 25px;
@@ -96,7 +77,7 @@ function submissionHandler() {
   border: 1px solid transparent;
   font-size: large;
 }
-.eventNameInput::placeholder {
+.eventNameInput::placeholder{
   font-size: large;
 }
 .eventNameInput:focus {
@@ -127,18 +108,14 @@ function submissionHandler() {
   width: 110px;
 }
 .dateLabel {
+  display: flex;
   width: 20%;
   height: auto;
   max-height: 10px;
+  align-items: center;
 }
 .saveButtonRow {
   display: flex;
   justify-content: flex-end;
-}
-.saveButton {
-  display: flex;
-  margin: 4px;
-  width: 50px;
-  justify-content: center;
 }
 </style>
