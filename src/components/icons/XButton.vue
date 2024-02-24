5b3e4f5dda45bf28b935acc61f83cb5172b03f51
Evan Lauer <lauerevan@gmail.com>
2024-02-24 15:37:29 -0600

    asdf

---
+++ src/components/icons/XButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <svg
+    class="xButton MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-dhaba5"
+    focusable="false"
+    aria-hidden="true"
+    viewBox="0 0 24 24"
+  >
+    <path
+      d="M19 6.41 17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"
+    ></path>
+  </svg>
+</template>
+<style scoped>
+.xButton {
+  height: 24px;
+  width: 24px;
+}
+</style>
